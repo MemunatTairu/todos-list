9ef027274a408c5c5657364b158c9e5604091c6e
MemunatTairu <tmemunat881@gmail.com>
2024-02-10 11:36:44 -0800

    project starter files

---
+++ Component/Todo.jsx
@@ -0,0 +1,7 @@
+import react from "react"
+const Todo = ()=> {
+    return <div>
+        <h1>TodosList</h1>
+    </div>
+}
+export default Todo; 
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Todo from '../Component/Todo'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Todo/>
+    </div>
+      
+  )
+}
+
+export default App
